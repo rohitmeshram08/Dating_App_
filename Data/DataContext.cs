6a17fcda79e7dc47348f1169868672909d7383e8
Rohit Meshram <rohitmeshram1608@gmail.com>
2024-12-05 01:07:06 +0530

    All the files are added to the Github Successfully
    Succes OK

---
+++ Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿
+using Dating_app_1.Entities;
+using Microsoft.EntityFrameworkCore;
+namespace Dating_app_1.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<App_User> AppUsers { get; set; }
+    }
+}
---
+++ Entities/App_User.cs
@@ -0,0 +1,10 @@
+﻿namespace Dating_app_1.Entities
+{
+    public class App_User
+    {
+        public int Id { get; set; }
+
+         public string UserName { get; set; }
+
+    }
+}
